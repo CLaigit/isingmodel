6b89d2054d36fda6995a1d0d2c4270ba44b52f5a
Chen Lai <littlelittlecc@gmail.com>
2016-05-21 23:05:54 -0700

    update

--- ising2d.cu
+++ ising2d.cu
@@ -1,8 +1,27 @@
 /*
 Ising model: Halmitonian H = /sum_ij J(sigma_i)(sigma_j)
-We set J = 1 first
-
 */
+
+/*
+* TODO:
+*   1. Calculate the energy in the program
+*   2. Calculate the heat capacity in the program
+*   3. Add more inputs to adjust the length of lattice
+*   4. A matlab code to plot data.
+*       data format example:
+*                    position.x  position.y   spin(-1, 1)
+*       Iteattion 1:    1           4               -1
+*                       *           *                *
+*                       *           *                *
+*       Iteattion 2:    4           3                1
+*                       *           *                *
+*                       *           *                *
+*       Iteattion N:    35          76               1
+*                       *           *                *
+*                       *           *                *
+*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,17 +29,33 @@ We set J = 1 first
 #include <curand.h>
 #include <curand_kernel.h>
 
+/*
+* LATTICE_LENGTH is the length of the lattice
+* LATTICE_LENGTH is the number of element is one lattice
+* BOLTZMANN_CONST is bolzmann constant. It is set to 1.
+*/
+
 #define  LATTICE_LENGTH 256
 #define  LATTICE_2 (LATTICE_LENGTH * LATTICE_LENGTH)
 #define  BOLTZMANN_CONST 1
-#define  BLOCK_SIZE 256
 #define  N LATTICE_LENGTH
 
 __device__ int energy(int up, int down, int left, int right, int center);
 __global__ void update(int *lattice, unsigned int offset);
 __global__ void printstate(int *lattice);
 
+
+/*
+*   update is the function to update a point
+*   1. flip a point (1 -> -1 or -1 -> 1)
+*   2. compare the energy before flip a point and after flip a point
+*   3. if the energy with flipped point is small, accept
+*   4. if the energy is larger, generate a random number pro_rand (0,1),
+*      if pro_rand < e^(-beta * delatE), aceept. else reject.
+*/
 __global__ void update(int* lattice, const unsigned int offset, double beta){
+    // Calculate the global index
+    // Calculate the global index for the up, down, left, right index.
     const unsigned int idx = blockIdx.x * blockDim.y + threadIdx.x + offset;
     const unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y + offset;
     const unsigned int idx_l = (idx - 1 + N) % N;
@@ -30,15 +65,14 @@ __global__ void update(int* lattice, const unsigned int offset, double beta){
     int flip, up, down, left, right, center;
     double pro_rand;
     double deltaE;
-    curandState_t state1;
-    curandState_t state2;
-    curand_init(idx, idx + 1, 0, &state1);
-    curand_init(idy, idy + 1, 0, &state2);
+
+    // To generate random number in cuda
+    curandState_t state;
+    curand_init(idx, idx + 1, 0, &state);
 
     if (idx < N && idy < N && idx_l < N && idx_r < N && idy_u < N && idy_d < N){
-
-        flip = 2 * (curand(&state1) % 2) - 1;
-        pro_rand = curand_uniform(&state2);
+        // generate a random number between (0,1) uniformly
+        pro_rand = curand_uniform(&state);
 
         up = lattice[idx + idy_u * N];
         down = lattice[idx + idy_d * N];
@@ -46,15 +80,25 @@ __global__ void update(int* lattice, const unsigned int offset, double beta){
         right = lattice[idx_r + idy * N];
         center = lattice[idx + idy * N];
 
+        // Flip the center element
+        flip = -center;
+        // Calculate the difference between these two state
         deltaE = energy(up, down, left, right, flip);
         deltaE -= energy(up, down, left, right, center);
 
+        // If deltaE < 0 or pro_rand <= e^(-beta * deltaE), accept new value
         if (pro_rand <= exp(- beta * deltaE)){
             lattice[idx + idy * N ] = flip;
         }
     }
 }
 
+/*
+*   printstate is the function to print the whole matrix.
+*   Since it prints in parallel, we also print the global
+*   index of the matrx.
+*   it prints (x, y, (1 or -1)).
+*/
 __global__ void printstate(int* lattice) {
     const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
     const unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
@@ -64,6 +108,11 @@ __global__ void printstate(int* lattice) {
     }
 }
 
+
+/*
+*   energy is the function used to calculate the energy between
+*   (center, up), (center, down), (center, left), (center, right)
+*/
 __device__ int energy(int up, int down, int left, int right, int center){
     double H;
     H = -up * center;
@@ -73,6 +122,11 @@ __device__ int energy(int up, int down, int left, int right, int center){
     return H;
 }
 
+/*
+*   Commandline inputs option
+*   1. Tempurature (T)
+*
+*/
 int main (int argc, char *argv[]){
 
     int *lattice;
@@ -88,27 +142,40 @@ int main (int argc, char *argv[]){
     int numblocksX = LATTICE_LENGTH / numthreadx;
     int numblocksY = LATTICE_LENGTH / numthready;
 
+    // First input: Tempurature. Usually between (1, 6),
+    // Critical Tempurature is around 2.2
     T = argc > 1 ? atof(argv[1]) : 2;
 
-    const size_t bytes = LATTICE_2 * sizeof(int);
-    lattice = (int*)malloc(LATTICE_2 * sizeof(int));
+    // Define the size of lattice and energy
+    const size_t bytes_lattice = LATTICE_2 * sizeof(int);
+    const size_t bytes_energy = sizeof(int);
 
+    // Allocate memory for lattice. It is a lattice^2 long array.
+    // The value can only be 1 or -1.
+    lattice = (int*)malloc(LATTICE_2 * sizeof(int));
+    // energy = (int*)malloc(sizeof(int));
+
+    // initialize lattice by rand(-1, 1)
     for(int i = 0; i < LATTICE_2; i++){
             lattice[i] = 2 * (rand() % 2) - 1;
             //lattice[i] = 1;
     }
 
-    // Tempurature
+    // Set dimensions of block and grid
     dim3 grid(numblocksX, numblocksY, 1);
     dim3 thread(numthreadx, numthready,1);
 
+    // beta is a parameter in the probability
     double beta = 1.0 / BOLTZMANN_CONST / T;
-    srand (time(NULL));
-    // Initialize every grid point
 
-    cudaMalloc((void **)&d_lattice, bytes);
-    cudaMemcpy(d_lattice, lattice, bytes, cudaMemcpyHostToDevice);
-    cudaDeviceSetLimit(cudaLimitPrintfFifoSize, 256 * 256 * sizeof(int) * 256);
+    // Allocate memoery in device and copy from host to device
+    cudaMalloc((void **)&d_lattice, bytes_lattice);
+    cudaMalloc((void **)&d_energy, bytes_energy);
+    cudaMemcpy(d_lattice, lattice, bytes_lattice, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_energy, energy, bytes_energy, cudaMemcpyHostToDevice);
+
+    // To change the buffer size of printf; otherwise it cannot print all data
+    cudaDeviceSetLimit(cudaLimitPrintfFifoSize, N * N * sizeof(int) * N);
 
     // Warmup process
     for (int iter = 0; iter < warmsteps; iter++){
@@ -119,7 +186,7 @@ int main (int argc, char *argv[]){
             fprintf(stderr,"Warmup Iteration: %d\n", iter);
     }
 
-    // Measure steps
+    // Measure process
     for (int nstep = 0; nstep < nout; nstep++){
         update<<<grid, thread>>>(d_lattice, 0, beta);
         update<<<grid, thread>>>(d_lattice, 1, beta);
