2506a9c1721d48523ed073de079edf390691a221
Chen Lai <littlelittlecc@gmail.com>
2016-05-27 15:16:58 -0700

    src

--- src/ising.c
+++ src/ising.c
@@ -8,11 +8,9 @@ We set J = 1 first
 #include <time.h>       /* time */
 
 #define  LATTICE_LENGTH 200
-#define  COLUMN LATTICE_LENGTH
-#define  ROW LATTICE_LENGTH
 #define  BOLTZMANN_CONST 1
 #define  WARMSTEPS 1e3
-#define  NSWEEPS 100
+#define  NSWEEPS 1000
 
 // Calculate the energy of the (up, center) (down, center) (left, center) ( right, center)
 double energy(int up, int down, int left, int right, int center){
@@ -38,22 +36,22 @@ int main (int argc, char *argv[]){
     double siteE = 0.0;
     srand (time(NULL));
     // Initialize every grid point
-    for (int i = 0; i < COLUMN; i++){
-        for(int j = 0; j < ROW; j++){
+    for (int i = 0; i < col; i++){
+        for(int j = 0; j < row; j++){
             lattice[i][j] = 2 * (rand() % 2) - 1;
             // lattice[i][j] = 1;
         }
     }
     // Warmup process
     for (int nstep = 0; nstep < WARMSTEPS; nstep++){
-        for(int i = 0; i < COLUMN; i++){
-            for(int j = 0; j < ROW; j++){
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
                 // flip a spin
                 // If the energy becomes small, accept it
                 // Else accept w.r.t the probability e^-beta*deltaE
                 new = -lattice[i][j];
-                deltaE = energy(lattice[ (i - 1 + ROW) % ROW][j], lattice[(i + 1 + ROW) % ROW][j], lattice[i][(j - 1 + ROW) % ROW], lattice[i][(j + 1 + ROW) % ROW], new);
-                deltaE -= energy(lattice[ (i - 1 + ROW) % ROW][j], lattice[(i + 1 + ROW) % ROW][j], lattice[i][(j - 1 + ROW) % ROW], lattice[i][(j + 1 + ROW) % ROW], lattice[i][j]);
+                deltaE = energy(lattice[ (i - 1 + row) % row][j], lattice[(i + 1 + row) % row][j], lattice[i][(j - 1 + row) % row], lattice[i][(j + 1 + row) % row], new);
+                deltaE -= energy(lattice[ (i - 1 + row) % row][j], lattice[(i + 1 + row) % row][j], lattice[i][(j - 1 + row) % row], lattice[i][(j + 1 + row) % row], lattice[i][j]);
                 if ((double)rand() / (double)RAND_MAX <= exp(- beta * deltaE)){
                     lattice[i][j] = new;
                 }
@@ -63,40 +61,45 @@ int main (int argc, char *argv[]){
 
     // Measure steps
     for (int nstep = 0; nstep < NSWEEPS; nstep++){
-        for(int i = 0; i < COLUMN; i++){
-            for(int j = 0; j < ROW; j++){
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
                 new = -lattice[i][j];
-                deltaE = energy(lattice[ (i - 1 + ROW) % ROW][j], lattice[(i + 1 + ROW) % ROW][j], lattice[i][(j - 1 + ROW) % ROW], lattice[i][(j + 1 + ROW) % ROW], new);
-                deltaE -= energy(lattice[ (i - 1 + ROW) % ROW][j], lattice[(i + 1 + ROW) % ROW][j], lattice[i][(j - 1 + ROW) % ROW], lattice[i][(j + 1 + ROW) % ROW], lattice[i][j]);
+                deltaE = energy(lattice[ (i - 1 + row) % row][j], lattice[(i + 1 + row) % row][j], lattice[i][(j - 1 + row) % row], lattice[i][(j + 1 + row) % row], new);
+                deltaE -= energy(lattice[ (i - 1 + row) % row][j], lattice[(i + 1 + row) % row][j], lattice[i][(j - 1 + row) % row], lattice[i][(j + 1 + row) % row], lattice[i][j]);
                 if (deltaE < 0 || (double)rand() / (double)RAND_MAX <= exp(- beta * deltaE)){
                     lattice[i][j] = new;
                 }
             }
         }
         tmpE = 0, tmpE2 = 0, tmpmag = 0, tmpmag2 = 0;
-        for(int i = 0; i < COLUMN; i++){
-            for(int j = 0; j < ROW; j++){
-                siteE += energy(lattice[ (i - 1 + ROW) % ROW][j], lattice[(i + 1 + ROW) % ROW][j], lattice[i][(j - 1 + ROW) % ROW], lattice[i][(j + 1 + ROW) % ROW], lattice[i][j])/2;
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
+                siteE = energy(lattice[ (i - 1 + row) % row][j], lattice[(i + 1 + row) % row][j], lattice[i][(j - 1 + row) % row], lattice[i][(j + 1 + row) % row], lattice[i][j])/2;
                 tmpE += siteE;
                 tmpE2 += siteE * siteE;
                 tmpmag += lattice[i][j];
                 tmpmag2 += lattice[i][j] * lattice[i][j];
             }
         }
-        averE += (1.0 * tmpE / LATTICE_LENGTH / LATTICE_LENGTH / NSWEEPS);
-        averE2 += (1.0 * tmpE2 / LATTICE_LENGTH / LATTICE_LENGTH / NSWEEPS);
-        avermag += (1.0 * tmpmag / LATTICE_LENGTH / LATTICE_LENGTH / NSWEEPS);
-        avermag2 += (1.0 * tmpmag2 / LATTICE_LENGTH / LATTICE_LENGTH / NSWEEPS);
+        averE += (1.0 * tmpE / col / row / NSWEEPS);
+        averE2 += (1.0 * tmpE2 / col / row / NSWEEPS);
+        avermag += (1.0 * tmpmag / col / row / NSWEEPS);
+        avermag2 += (1.0 * tmpmag2 /col / row / NSWEEPS);
         // Output data every NOUT
-        // for(int i = 0; i < COLUMN; i++){
-        //     for(int j = 0; j < COLUMN-1; j++){
-        //         printf("%d,", lattice[i][j]);
-        //     }
-        //     printf("%d\n", lattice[i][COLUMN-1]);
-        // }
+#ifdef PICORE
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < col-1; j++){
+                printf("%d,", lattice[i][j]);
+            }
+            printf("%d\n", lattice[i][col-1]);
+        }
+    }
+#else
     }
     printf("%f\n", T);
     printf("%f\n", averE);
     printf("%f\n", 1.0*(averE2 - averE * averE) / T / T);
+    printf("%f\n", avermag);
     printf("%f\n", 1.0*(avermag2 - avermag * avermag) / T );
+#endif
 }
