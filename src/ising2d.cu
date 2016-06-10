089044095925e3bc398b50e47bc31d18d5e01965
Chen Lai <littlelittlecc@gmail.com>
2016-06-10 01:01:58 -0700

    add heat capacity, mag, mag2

--- src/ising2d.cu
+++ src/ising2d.cu
@@ -87,8 +87,6 @@ __global__ void update(int* lattice, const unsigned int offset, double beta, cur
 
     if (idx < N && idy < N && idx_l < N && idx_r < N && idy_u < N && idy_d < N){
         if( ((idx + idy) % 2 == 0 && offset == 0) || ((idx + idy) % 2 == 1 && offset == 1) ){
-            // generate a random number between (0,1) uniformly
-            // pro_rand = curand_uniform(&state);
 
             up = lattice[idx + idy_u * N];
             down = lattice[idx + idy_d * N];
@@ -105,7 +103,6 @@ __global__ void update(int* lattice, const unsigned int offset, double beta, cur
             // If deltaE < 0 or pro_rand <= e^(-beta * deltaE), accept new value
             if (pro_rand <= exp(- beta * deltaE)){
                 lattice[idx + idy * N ] = flip;
-                // energy[idx + idy * N] += 1.0 * deltaE / TIME_LENGTH;
             }
         }
     }
@@ -126,7 +123,6 @@ __global__ void printstate(double* energy) {
     }
 }
 
-
 /*
 *   energy is the function used to calculate the energy between
 *   (center, up), (center, down), (center, left), (center, right)
@@ -135,7 +131,8 @@ __device__ double local_energy(int up, int down, int left, int right, int center
     return -center * (up + down + left + right);
 }
 
-__global__ void updateEnergy(int* lattice, double* energy, int init){
+__global__ void updateEnergy(int* lattice, double* energy, double* energy2, double* mag,double* mag2, int init){
+
     const unsigned int idx = blockIdx.x * blockDim.y + threadIdx.x;
     const unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
     const unsigned int idx_l = (idx - 1 + N) % N;
@@ -143,6 +140,7 @@ __global__ void updateEnergy(int* lattice, double* energy, int init){
     const unsigned int idy_u = (idy - 1 + N) % N;
     const unsigned int idy_d = (idy + 1 + N) % N;
     int up, down, left, right, center;
+    double site_E;
 
     up = lattice[idx + idy_u * N];
     down = lattice[idx + idy_d * N];
@@ -151,11 +149,19 @@ __global__ void updateEnergy(int* lattice, double* energy, int init){
     center = lattice[idx + idy * N];
 
     if (idx < N && idy < N){
+        site_E = local_energy(up, down, left, right, center) / 2.0;
+
         if(init == 1){
-            energy[idx + N * idy] = 1.0 * local_energy(up, down, left, right, center) / (TIME_LENGTH + 1);
+            energy[idx + N * idy] = 1.0 * site_E / (TIME_LENGTH + 1);
+            energy2[idx + N * idy] = 1.0 * site_E * site_E / (TIME_LENGTH + 1);
+            mag[idx + N * idy] = 1.0 * center / (TIME_LENGTH + 1);
+            mag2[idx + N * idy] = 1.0 * center * center / (TIME_LENGTH + 1);
         }
         else{
-            energy[idx + N * idy] += 1.0 * local_energy(up, down, left, right, center) / (TIME_LENGTH + 1);
+            energy[idx + N * idy] += 1.0 * site_E / (TIME_LENGTH + 1);
+            energy2[idx + N * idy] += 1.0 * site_E * site_E / (TIME_LENGTH + 1);
+            mag[idx + N * idy] += 1.0 * center / (TIME_LENGTH + 1);
+            mag2[idx + N * idy] += 1.0 * center * center / (TIME_LENGTH + 1);
         }
     }
 }
@@ -173,11 +179,15 @@ int main (int argc, char *argv[]){
     double *energy;
     double *d_energy;
 
-    // double *energy2;
-    // double *d_energy2;
+    double *energy2;
+    double *d_energy2;
 
-    double *random;
-    double *d_random;
+    double *mag2;
+    double *d_mag2;
+
+    double *mag2;
+    double *d_mag2;
+
     curandState *d_states;
 
     double T = 2;
@@ -195,22 +205,24 @@ int main (int argc, char *argv[]){
     T = argc > 1 ? atof(argv[1]) : 2;
 
     // Define the size of lattice and energy
-    const size_t bytes_lattice = LATTICE_2 * sizeof(int);
-    const size_t bytes_energy = LATTICE_2 * sizeof(double);
-    const size_t bytes_random = LATTICE_2 * sizeof(double);
+    const size_t bytes_int = LATTICE_2 * sizeof(int);
+    const size_t bytes_double = LATTICE_2 * sizeof(double);
 
     // Allocate memory for lattice. It is a lattice^2 long array.
     // The value can only be 1 or -1.
     lattice = (int*)malloc(LATTICE_2 * sizeof(int));
     energy = (double*)malloc(LATTICE_2 * sizeof(double));
-    random = (double*)malloc(LATTICE_2 * sizeof(double));
+    energy2 = (double*)malloc(LATTICE_2 * sizeof(double));
+    mag = (double*)malloc(LATTICE_2 * sizeof(double));
+    mag2 = (double*)malloc(LATTICE_2 * sizeof(double));
 
     // initialize lattice by rand(-1, 1)
     for(int i = 0; i < LATTICE_2; i++){
         lattice[i] = 2 * (rand() % 2) - 1;
         energy[i] = 0.0;
-        // energy2[i] = 0.0;
-        random[i] = (double)rand() / (double)RAND_MAX;
+        energy2[i] = 0.0;
+        mag[i] = 0.0;
+        mag2[i] = 0.0;
     }
 
     // Set dimensions of block and grid
@@ -221,72 +233,75 @@ int main (int argc, char *argv[]){
     double beta = 1.0 / BOLTZMANN_CONST / T;
 
     // Allocate memoery in device and copy from host to device
-    cudaMalloc((void **)&d_lattice, bytes_lattice);
-    cudaMalloc((void **)&d_energy, bytes_energy);
-    // cudaMalloc((void **)&d_energy2, bytes_energy);
-    cudaMalloc((void **)&d_random, bytes_random);
-    cudaMalloc((void **)&d_states, LATTICE_2 * sizeof(curandState));
+    cudaMalloc((void **)&d_lattice, bytes_int);
+    cudaMalloc((void **)&d_energy, bytes_double);
+    cudaMalloc((void **)&d_energy2, bytes_double);
+    cudaMalloc((void **)&d_mag, bytes_double);
+    cudaMalloc((void **)&d_mag2, bytes_double);
 
-    cudaMemcpy(d_lattice, lattice, bytes_lattice, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_energy, energy, bytes_energy, cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_energy2, energy2, bytes_energy2, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_random, random, bytes_random, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_lattice, lattice, bytes_int, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_energy, energy, bytes_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_energy2, energy2, bytes_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mag, mag, bytes_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mag2, mag2, bytes_double, cudaMemcpyHostToDevice);
 
     // To change the buffer size of printf; otherwise it cannot print all data
     cudaDeviceSetLimit(cudaLimitPrintfFifoSize, N * N * sizeof(int) * N);
 
-
     ini_rng<<<grid, thread>>>(d_states, time(NULL));
 
     // Warmup process
-
     for (int iter = 0; iter < warmsteps; iter++){
-        for(int i = 0; i < LATTICE_2; i++){
-            random[i] = (double)rand() / (double)RAND_MAX;
-        }
-        cudaMemcpy(d_random, random, bytes_random, cudaMemcpyHostToDevice);
-        // update_random<<<grid, thread>>>(d_lattice, d_random, 0, beta);
-        // update_random<<<grid, thread>>>(d_lattice, d_random, 1, beta);
-        // update<<<grid, thread>>>(d_lattice, 0, beta, iter);
-        // update<<<grid, thread>>>(d_lattice, 1, beta, iter);
         update<<<grid, thread>>>(d_lattice, 0, beta, d_states);
         update<<<grid, thread>>>(d_lattice, 1, beta, d_states);
         // cudaDeviceSynchronize();
-        if(iter % warp == 0)
-            fprintf(stderr,"Warmup Iteration: %d\n", iter);
     }
-    updateEnergy<<<grid, thread>>>(d_lattice, d_energy, 1);
+    updateEnergy<<<grid, thread>>>(d_lattice, d_energy, d_energy2, d_mag, d_mag2, 1);
     // Measure process
     for (int nstep = 0; nstep < nout; nstep++){
-        for(int i = 0; i < LATTICE_2; i++){
-            random[i] = (double)rand() / (double)RAND_MAX;
-        }
-        cudaMemcpy(d_random, random, bytes_random, cudaMemcpyHostToDevice);
-        // update_random<<<grid, thread>>>(d_lattice, d_random, 0, beta);
-        // update_random<<<grid, thread>>>(d_lattice, d_random, 1, beta);
-        // update<<<grid, thread>>>(d_lattice, 0, beta, nstep);
-        // update<<<grid, thread>>>(d_lattice, 1, beta, nstep);
         update<<<grid, thread>>>(d_lattice, 0, beta, d_states);
         update<<<grid, thread>>>(d_lattice, 1, beta, d_states);
-
-        updateEnergy<<<grid, thread>>>(d_lattice, d_energy, 0);
-        if(nstep % warp == 0)
-            fprintf(stderr,"Measure Iteration: %d\n", nstep);
+        updateEnergy<<<grid, thread>>>(d_lattice, d_energy, d_energy2, d_mag, d_mag2, 0);
     }
     // printstate<<<grid, thread>>>(d_energy);
-    cudaMemcpy(energy, d_energy, bytes_energy, cudaMemcpyDeviceToHost);
-    // cudaMemcpy(energy2, d_energy2, bytes_energy2, cudaMemcpyDeviceToHost);
+    cudaMemcpy(energy, d_energy, bytes_double, cudaMemcpyDeviceToHost);
+    cudaMemcpy(energy2, d_energy2, bytes_double2, cudaMemcpyDeviceToHost);
+    cudaMemcpy(mag, d_mag, bytes_mag, cudaMemcpyDeviceToHost);
+    cudaMemcpy(mag2, d_mag2, bytes_mag2, cudaMemcpyDeviceToHost);
 
-    double sum = 0.0;
+    double sum_E = 0.0;
+    double sum_E2 = 0.0;
+    double sum_site = 0.0;
+    double sum_site2 = 0.0;
     // double sum2 = 0.0;
 
     for (int i = 0; i < N ; i++){
         for (int j = 0; j < N; j++){
-            sum += energy[i + j * N];
-            // sum2 += energy2[i + j * N];
+            sum_E += energy[i + j * N];
+            sum_E2 += energy2[i + j * N];
+            sum_site += mag[i + j * N];
+            sum_site2 += mag2[i + j * N];
         }
     }
-    printf("%f\n", 0.5 * sum / LATTICE_2);
+
+    double aver_E = 1.0 * sum_E / LATTICE_2;
+    double aver_E2 = 1.0 * sum_E2 / LATTICE_2;
+    double aver_site = 1.0 * sum_site / LATTICE_2;
+    double aver_site2 = 1.0 * sum_site2 / LATTICE_2;
+
+    double heat_capacity = 1.0 * (aver_E2 - aver_E * aver_E) / T / T;
+    double mag_sus = 1.0 * (aver_site2 - aver_site * aver_site) / T;
+
+    printf("%f\n", T);
+    printf("%d\n", LATTICE_LENGTH);
+    printf("%f\n", aver_E);
+    printf("%f\n", heat_capacity);
+    printf("%f\n", fabs(aver_site));
+    printf("%f\n", mag_sus );
+
+    //
+    // printf("%s\n", );
+    // printf("%f\n", 0.5 * sum / LATTICE_2);
     // printstate<<<grid, thread>>>(d_energy);
 
     free(lattice);
@@ -295,6 +310,8 @@ int main (int argc, char *argv[]){
     free(energy);
     cudaFree(d_energy);
 
-    free(random);
-    cudaFree(d_random);
+    free(energy);
+    cudaFree(d_energy);
+    free(energy);
+    cudaFree(d_energy);
 }
